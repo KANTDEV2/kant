1bc9b474d0567d7e9eb0aa0175bf6c300a94d6f2
KANTDEV2 <85336350+KANTDEV2@users.noreply.github.com>
2021-06-04 05:43:29 -0300

    Create index.js

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `𝙁𝙡𝙬 @${num.split('@')[0]} 😣✋`
+				teks = `VAI PELAS SOMBRAS @${num.split('@')[0]} FUCK`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			}
