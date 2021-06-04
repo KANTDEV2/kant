4c8145f8dbdc78f095dd15e94f057bc306c2c335
KANTDEV2 <kantmods4@gmail.com>
2021-06-04 04:32:08 -0300

    First commit

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*KANT* :  NATZIN
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*AÍ SIM PAE, VOCÊ É UM MEMBRO VIP😎✋`
+}
+exports.cekvip = cekvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 KANT〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/5571982507697*
+║
+╚═〘 KANT BOT 〙
+`
+}
+exports.iklan = iklan
